e347a9fc9cbeaafd07a0a249ed4454f9f950e9c4
Jurgen Jacobsen <jurgenjacobsen2005@gmail.com>
2021-08-21 19:13:07 -0300

    Added docs and markdown support

--- src/sub-servers/app.ts
+++ src/sub-servers/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import marked from 'marked';
 import { renderFile } from 'ejs';
 import { join } from 'path';
+import { readFileSync } from 'fs';
+import { markdownTemplate } from '../utils';
 
 const app = express();
+
+let docsPath = join(__dirname, '../../docs');
 
 app.set('views', join(__dirname, '../../pages'));
 app.set('view engine', 'ejs');
@@ -21,8 +26,15 @@ app.get('/search'); // Searches for a user or post
 app.get('/explore'); // Feed of global posts
 
 // Bureaucratic
-app.get('/terms'); // Terms of service and privacy
+app.get('/terms', (req, res) => {
+  let file = readFileSync(`${docsPath}/terms.md`);
+  res.send(markdownTemplate(`Terms of Service`, `/terms`, marked(file.toString())));
+}); // Terms of service and privacy
 
 app.get('/guidelines'); // Community and user behavior guidelines
+
+app.get('/dsc', (req, res) => {
+  res.redirect('https://discord.gg/GtaxXxNYaD');
+});
 
 export { app };
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,7 @@
 import colors from 'colors';
 import { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { Snowflake, User } from './database';
-import { SnowflakeUtil } from 'discord.js';
+import marked from 'marked';
 
 dotenv.config();
 
@@ -38,4 +37,89 @@ export function token(length: number) {
     b[i] = a[j as any];
   }
   return b.join('');
+}
+
+export function markdownTemplate(title: string, path: string, stringFile: string): string {
+  return `
+  <html>
+    <head>
+        <title>${title}</title>
+        <meta name="theme-color" content="#1c1c1c">
+        <meta property=”og:type” content=”website”/>
+        <meta property="og:title" content="${title}">
+        <meta property="og:url" content="https://dema.city/${path}">
+        <meta property="og:description" content="Welcome to Dema City, a multiporpose plataform.">
+        <meta property="og:image" content="/images/DemaLogo.png">
+
+        <link rel="shortcut icon" href="/assets/images/favicon.ico"/>
+        <link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
+        <style>
+          @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900;&display=swap");
+
+          * {
+            font-family: 'Poppins', sans-serif;
+          }
+
+          body {
+            background: #1c1c1c;
+            color: #FFF;
+            margin: 0;
+            padding: 0;
+            font-family: 'Poppins', sans-serif;
+          }
+
+          .container {
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            padding-top: 30px;
+            margin-right: auto;
+            margin-left: auto;
+          }
+
+          @media (min-width: 576px) {
+            .container {
+                max-width: 540px
+            }
+          }
+          
+          @media (min-width: 768px) {
+              .container {
+                  max-width: 720px;
+              }
+          }
+          
+          @media (min-width: 992px) {
+              .container {
+                  max-width: 960px;
+              }
+          }
+          
+          @media (min-width: 1200px) {
+              .container {
+                  max-width: 1140px;
+              }
+          }
+
+					a {
+						text-decoration: none;
+						color: #8855ff;
+					}
+					h1, h2, h3 {
+						font-weight: 100;
+					}
+					hr {
+						border: 1px solid #2C2F33;
+					}
+				</style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="container">
+          ${marked(stringFile)}
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
 }
